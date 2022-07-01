10e085b24f97657784e1aa54662585d39bfb67bc
Shawn McManus <s.mcmanus@obscuritylabs.com>
2022-07-01 18:15:36 +0000

    fix: fix directory

---
+++ darkmoon/src/darkmoon/__init__.py
@@ -0,0 +1,6 @@
+# Copyright (C) 2022 Obscurity Labs LLC. <admin@obscuritylabs.com> - All Rights Reserved
+# Unauthorized copying of this file, via any medium is strictly prohibited.
+# All rights reserved. No warranty, explicit or implicit, provided.
+# Proprietary and confidential.
+
+__version__ = 1
---
+++ darkmoon/src/darkmoon/main.py
@@ -0,0 +1,19 @@
+# example from https://fastapi.tiangolo.com/#create-it
+
+from typing import Any, Union
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root() -> Any:
+    """Fast API example."""
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None) -> Any:
+    """Fast API example."""
+    return {"item_id": item_id, "q": q}
