6f84c7c1ba1e29dd1f060fd8e740bb0ecbb22f0a
Donald Riley <d.riley@obscuritylabs.com>
2022-07-05 20:24:39 +0000

    docs: Took away unneed Uvicorn code

--- darkmoon/src/darkmoon/main.py
+++ darkmoon/src/darkmoon/main.py
@@ -2,11 +2,7 @@
 
 from typing import Any, Union
 
-import uvicorn
 from fastapi import FastAPI
-
-if __name__ == "__main__":
-    uvicorn.run("server.app:app", host="0.0.0.0", port=8000, reload=True)
 
 app = FastAPI()
 
