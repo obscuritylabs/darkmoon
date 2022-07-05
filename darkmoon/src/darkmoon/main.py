a789696cb61d958364f2be13cb206270ec757a24
Donald Riley <d.riley@obscuritylabs.com>
2022-07-05 20:20:37 +0000

    docs: added Uvicorn on main.py

--- darkmoon/src/darkmoon/main.py
+++ darkmoon/src/darkmoon/main.py
@@ -2,7 +2,11 @@
 
 from typing import Any, Union
 
+import uvicorn
 from fastapi import FastAPI
+
+if __name__ == "__main__":
+    uvicorn.run("server.app:app", host="0.0.0.0", port=8000, reload=True)
 
 app = FastAPI()
 
