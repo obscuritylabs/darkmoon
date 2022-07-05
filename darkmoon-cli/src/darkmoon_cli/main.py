f6ec4da913711d9cde70f7fbbae20072cf768e64
Pierre Tran <p.tran@obscuritylabs.com>
2022-07-05 20:44:29 +0000

    feat: iterates over files in a folder and get metadata using os

--- darkmoon-cli/src/darkmoon_cli/main.py
+++ darkmoon-cli/src/darkmoon_cli/main.py
@@ -1,9 +1,11 @@
 # example from https://typer.tiangolo.com/typer-cli/#sample-script
+import os
 from typing import Optional
 
 import typer
 
 app = typer.Typer()
+path = "/Users/pierre.tran/Documents/testing"
 
 
 @app.command()
@@ -22,6 +24,20 @@ def bye(name: Optional[str] = None) -> None:
         typer.echo(f"Bye {name}")
     else:
         typer.echo("Goodbye!")
+
+
+@app.command()
+def test():
+
+    allFiles = os.listdir("/workspaces/darkmoon/darkmoon-cli/src/darkmoon_cli/testing")
+    for file in allFiles:
+
+        print("size:" + str(os.path.getsize(os.getcwd() + "/testing/" + file)))
+        print(file + "metadata:")
+        print("last motified:")
+        print(str(os.path.getmtime(os.getcwd() + "/testing/" + file)))
+        print("creation date:")
+        print(str(os.path.getctime(os.getcwd() + "/testing/" + file)))
 
 
 if __name__ == "__main__":
