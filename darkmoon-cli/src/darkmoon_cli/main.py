8188340cdbcbf1a76b55f844e6a66a5e763e2259
Pierre Tran <p.tran@obscuritylabs.com>
2022-07-06 15:53:20 +0000

    feat: iterate over files and extract metadata using pathlib

--- darkmoon-cli/src/darkmoon_cli/main.py
+++ darkmoon-cli/src/darkmoon_cli/main.py
@@ -1,11 +1,11 @@
 # example from https://typer.tiangolo.com/typer-cli/#sample-script
 import os
+from pathlib import Path
 from typing import Optional
 
 import typer
 
 app = typer.Typer()
-path = "/Users/pierre.tran/Documents/testing"
 
 
 @app.command()
@@ -26,18 +26,37 @@ def bye(name: Optional[str] = None) -> None:
         typer.echo("Goodbye!")
 
 
+# function to iterate over files using os
 @app.command()
 def test():
 
-    allFiles = os.listdir("/workspaces/darkmoon/darkmoon-cli/src/darkmoon_cli/testing")
-    for file in allFiles:
-
+    all_files = os.listdir("/workspaces/darkmoon/darkmoon-cli/src/darkmoon_cli/testing")
+    for file in all_files:
         print("size:" + str(os.path.getsize(os.getcwd() + "/testing/" + file)))
         print(file + "metadata:")
         print("last motified:")
         print(str(os.path.getmtime(os.getcwd() + "/testing/" + file)))
         print("creation date:")
         print(str(os.path.getctime(os.getcwd() + "/testing/" + file)))
+
+
+# function to iterate over files using pathlib
+@app.command()
+def path():
+
+    p = Path("/workspaces/darkmoon/darkmoon-cli/src/darkmoon_cli/testing")
+    size = p.stat().st_size
+    print(size)
+    for file in p.iterdir():
+        t = Path(file)
+        print("\n")
+        print(t.stem)
+        print("size:")
+        print(t.stat().st_size)
+        print("last motified:")
+        print(t.stat().st_atime)
+        print("creation time:")
+        print(t.stat().st_ctime)
 
 
 if __name__ == "__main__":
