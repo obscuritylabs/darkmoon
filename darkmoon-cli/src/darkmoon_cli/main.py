8987401f5f5276a5b9723024a16356eb98a15645
Pierre Tran <p.tran@obscuritylabs.com>
2022-07-08 20:00:32 +0000

    fix: fixed iterate function and made metadata output to terminal

--- darkmoon-cli/src/darkmoon_cli/main.py
+++ darkmoon-cli/src/darkmoon_cli/main.py
@@ -81,8 +81,6 @@ def get_metadata(path: Path) -> str:
     source_iso_data = source_iso()
 
     # rich PE header hash
-    if extension == ".exe":
-        rich_pe_header(curr_filename)
 
     # print statements used for testing
     print("name:" + curr_filename)
@@ -90,6 +88,12 @@ def get_metadata(path: Path) -> str:
     print("hashes:" + str(hash_list))
     print("OS:" + operating_system)
     print("ISO:" + source_iso_data)
+
+    # rich PE header hash
+    if extension == ".exe":
+        pe_header = rich_pe_header(path)
+        print("rich_pe_header_hash:" + str(pe_header))
+    print("\n")
 
     json = ""
 
@@ -110,7 +114,7 @@ def iterate():
             None
 
     """
-    root = Path("/testing")
+    root = Path("/workspaces/darkmoon/darkmoon-cli/src/darkmoon_cli/testing")
 
     queue = []
     queue.append(root)
@@ -121,12 +125,10 @@ def iterate():
 
         for files in m.glob("*"):
             print(files)
-            if files.is_dir():
-                queue.append(files)
-            else:
+            if files.is_file():
                 get_metadata(files)
-
-    # iterate over files and going into folders given root
+            else:
+                queue.append(files)
 
 
 @app.command()
@@ -142,23 +144,23 @@ def hashes(path: Path) -> list[str]:
             list
 
     """
-    filename = str(path.name)
+    all_hashes = []
     h_md5 = hashlib.md5()
     h_sha1 = hashlib.sha1()
     h_sha256 = hashlib.sha256()
     h_sha512 = hashlib.sha512()
 
-    with open(filename, "rb") as file:
+    with open(path, "rb") as file:
         # read file in chunks and update hash
-        chunk = b""
-        while chunk != b"":
-            chunk = file.read(1024)
-            h_md5.update(chunk)
-            h_sha1.update(chunk)
-            h_sha256.update(chunk)
-            h_sha512.update(chunk)
+        while True:
+            data = file.read(1024)
+            if not data:
+                break
+            h_md5.update(data)
+            h_sha1.update(data)
+            h_sha256.update(data)
+            h_sha512.update(data)
 
-    all_hashes = []
     all_hashes.append(h_md5.hexdigest())
     all_hashes.append(h_sha1.hexdigest())
     all_hashes.append(h_sha256.hexdigest())
@@ -183,7 +185,7 @@ def source_iso() -> str:
 
 
 @app.command()
-def rich_pe_header(exe_file: str) -> list[str]:
+def rich_pe_header(exe_file: Path) -> list[str]:
     """
     Get a list of rich PE hash headers.
 
