5cd7d5f65e63cf2c47dcb0b52566d4753838e157
Pierre Tran <p.tran@obscuritylabs.com>
2022-07-18 15:46:20 +0000

    feat: get metadata by file_name, file_hash, or list all metadata put into one get endpoint, get metadata by id

--- darkmoon/src/darkmoon/main.py
+++ darkmoon/src/darkmoon/main.py
@@ -4,6 +4,8 @@
 # IMPORTS #
 ###########
 
+from typing import Optional
+
 from fastapi import FastAPI
 from motor.motor_asyncio import AsyncIOMotorClient
 
@@ -25,21 +27,45 @@ app = FastAPI()
 
 
 @app.get("/metadata")
-async def all_metadata() -> list[MetadataEntity]:
-    """Return all metadata stored in the mongodb server.
+async def list_metadata(file_name: Optional[str] = None, hash: Optional[str] = None) -> list[MetadataEntity]:
+    """Return list of metadata that matches the parameters.
 
     Parameters:
-        None
+        file_name: The name of the file being searched. Is None by default
+        hash: Hash of the file. Is None by default.
     Returns:
-        'list[MetadataEntity]': Lists of all documents in server.
+        documents: List of all documents that match parameters
 
     """
     documents = []
-    async for doc in collection.find():
+    search = {}
+    if file_name:
+        search["name"] = file_name
+    if hash:
+        search["hashes"] = hash
+    async for doc in collection.find(search):
         doc["id"] = str(doc["_id"])
         documents.append(MetadataEntity(**doc))
-
     return documents
+
+
+@app.get("/metadata/{id}")
+async def get_metadata_by_id(id: str) -> list[MetadataEntity]:
+    """Return file by ObjectID in MongoDB.
+
+    Parameters:
+        id: Unique id of specific entry in MongoDB
+    Returns:
+        documents: Returns the entry with matching id
+
+    """
+    document = []
+    print("outside hello")
+    async for doc in collection.find({"_id": id}):
+        doc["id"] = str(doc["_id"])
+        document.append(MetadataEntity(**doc))
+
+    return document
 
 
 @app.post("/metadata")
@@ -47,7 +73,7 @@ async def upload_metadata(file: Metadata) -> None:
     """Fast API POST function for incoming files.
 
     Parameters:
-        file - The file that is uploaded to the database.
+        file :The file that is uploaded to the database.
     Returns:
        None
 
