ed7d4e0d03fd20077468437bf777b04aa46ba3d9
Pierre Tran <p.tran@obscuritylabs.com>
2022-07-22 16:54:56 +0000

    feat: calls get_all_exe_metadata funtion from get_all_metadata, remove unused exe metadata functions

--- darkmoon-cli/src/darkmoon_cli/main.py
+++ darkmoon-cli/src/darkmoon_cli/main.py
@@ -74,26 +74,7 @@ def get_metadata(path: Path) -> None:
     try:
 
         if extension == ".exe" or extension == ".dll":
-            pe_header = get_header_hashes(path)
-            print("rich_pe_header_hash: " + str(pe_header))
-            pe_sig = get_header_sig(path)
-            print("PE Signature: " + str(pe_sig))
-            pe_timestamp = get_timestamp(path)
-            print("PE_Timestamp: " + str(pe_timestamp))
-            pe_mach = get_machine(path)
-            print("Machine: " + str(pe_mach))
-            pe_comptime = get_compile_time(path)
-            print("Compile Time: " + str(pe_comptime))
-
-            exe_metadata = {
-                "machine_type": pe_mach,
-                "timestamp": pe_timestamp,
-                "compile_time": pe_comptime,
-                "signature": pe_sig,
-                "rich_header_hashes": {},
-            }
-
-            data_fields["header_info"] = exe_metadata
+            data_fields["header_info"] = get_all_exe_metadata(path)
 
     except (PEFormatError):
         print("This program cannot read an NE file.")
@@ -181,14 +162,17 @@ def get_all_exe_metadata(exe_file: Path) -> dict[str, Any]:
     binary_hash = ["md5", "sha1", "sha256", "sha512"]
     for hash in binary_hash:
         all_header_hash[hash] = pe_obj.get_rich_header_hash(algorithm=hash)
+    print("rich_pe_header_hash: " + str(all_header_hash))
 
     # header_signature
     sig = str(pe_obj.NT_HEADERS)
     sig_list = sig.split()
     signature = sig_list[sig_list.index("Signature:") + 1]
+    print("PE Signature: " + str(signature))
 
     # compile_time
     compile_time = "Time to compile file"
+    print("compile_time: " + compile_time)
 
     # timestamp
     file_header = str(pe_obj.FILE_HEADER)
@@ -196,9 +180,11 @@ def get_all_exe_metadata(exe_file: Path) -> dict[str, Any]:
     timestamp = str(file_header_list[file_header_list.index("TimeDateStamp:") + 1])
     for num in range(2, 8):
         timestamp += str(file_header_list[file_header_list.index("TimeDateStamp:") + num])
+    print("PE_Timestamp: " + str(timestamp))
 
     # machine_type
     machine = file_header_list[file_header_list.index("Machine:") + 1]
+    print("machine_type: " + str(machine))
 
     exe_metadata = {
         "machine_type": machine,
@@ -207,108 +193,8 @@ def get_all_exe_metadata(exe_file: Path) -> dict[str, Any]:
         "signature": signature,
         "rich_header_hashes": all_header_hash,
     }
-    print(exe_metadata)
+
     return exe_metadata
-
-
-@app.command()
-def get_header_hashes(exe_file: Path) -> list[str]:
-    """
-    Get a list of rich PE hash headers.
-
-    Uses pefile library.
-
-        Parameters:
-            exe_file (Path): Path to an exe file.
-        Returns:
-            all_header_hash (list[str]): List of all header hashes.
-    """
-    # check that it is .exe in main func using glob
-    pe_obj = pefile.PE(exe_file)
-
-    binary_hash = ["md5", "sha1", "sha256", "sha512"]
-    all_header_hash = []
-    # adds all PE rich headers to a list
-
-    for hash in binary_hash:
-        all_header_hash.append(pe_obj.get_rich_header_hash(algorithm=hash))
-
-    print(all_header_hash)
-    return all_header_hash
-
-
-@app.command()
-def get_header_sig(exe_file: Path) -> str:
-    """
-    Get the signature of the .exe file.
-
-    Uses pefile library.
-
-        Parameters:
-            exe_file (Path): Path to an exe file.
-        Returns:
-            signature (str): The signature of the exe file.
-    """
-    sig = pefile.PE(exe_file)
-    sig = str(sig.NT_HEADERS)
-    sig_list = sig.split()
-    signature = sig_list[sig_list.index("Signature:") + 1]
-    return signature
-
-
-@app.command()
-def get_timestamp(exe_file: Path) -> str:
-    """
-    Get the timestamp of the .exe file.
-
-    Uses pefile library.
-
-        Parameters:
-            exe_file (Path): Path to an exe file.
-        Returns:
-            timestamp (str): The timestamp of the exe file.
-    """
-    time = pefile.PE(exe_file)
-    time = str(time.FILE_HEADER)
-    time_list = time.split()
-    timestamp = str(time_list[time_list.index("TimeDateStamp:") + 1])
-    for num in range(2, 8):
-        timestamp += str(time_list[time_list.index("TimeDateStamp:") + num])
-
-    return timestamp
-
-
-@app.command()
-def get_compile_time(exe_file: Path) -> str:
-    """
-    Get the compile time of the .exe file.
-
-        Parameters:
-            exe_file (Path): Path to an exe file.
-        Returns:
-            compile_time (str): The compile time of the exe file.
-    """
-    compile_time = "Time to compile file"
-    return compile_time
-
-
-@app.command()
-def get_machine(exe_file: Path) -> str:
-    """
-    Get the architecture of the .exe file.
-
-    Uses pefile library.
-
-        Parameters:
-            exe_file (Path): Path to an exe file.
-        Returns:
-            machine (str): The machine type of the exe file.
-    """
-    arch = pefile.PE(exe_file)
-    arch = str(arch.FILE_HEADER)
-    arch_list = arch.split()
-    machine = arch_list[arch_list.index("Machine:") + 1]
-    return machine
 
 
 @app.command()
