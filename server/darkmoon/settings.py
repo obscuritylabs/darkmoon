516e11b5c3df1187b588be1be1e9c06029f60e05
Donald Riley <djrileyiii@icloud.com>
2023-05-24 15:27:29 -0400

    chore: changed PASSWORD from str to SecretStr. Removed unneccessary code

--- server/darkmoon/settings.py
+++ server/darkmoon/settings.py
@@ -8,7 +8,7 @@
 from ipaddress import IPv4Address
 
 from dotenv import load_dotenv
-from pydantic import BaseSettings
+from pydantic import BaseSettings, SecretStr
 
 #############
 # FUNCTIONS #
@@ -25,10 +25,10 @@ class Settings(BaseSettings):
     """Settings class."""
 
     SCHEME: str
-    IP_ADDRESS: IPv4Address
+    IPv4: IPv4Address
     PORT: int
     USERNAME: str
-    PASSWORD: str
+    PASSWORD: SecretStr
 
     class Config:
         """Config class."""
@@ -36,9 +36,13 @@ class Settings(BaseSettings):
         env_file = ".env"
         env_file_encoding = "utf-8"
 
+        json_encoders = {
+            SecretStr: lambda v: v.get_secret_value() if v else None,
+        }
+
     def mongo_DSN(self) -> str:
         """DSN_Model class."""
-        return f"{self.SCHEME}://{self.USERNAME}:{self.PASSWORD}@{self.IP_ADDRESS}:{self.PORT}/?authMechanism=DEFAULT"
+        return f"{self.SCHEME}://{self.USERNAME}:{self.PASSWORD.get_secret_value()}@{self.IPv4}:{self.PORT}/?"
 
 
 #############
