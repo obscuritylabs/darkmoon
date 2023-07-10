d48eb4af352663301e0591d4aad237f961db3370
Viraj <v.ganguli@obscuritylabs.com>
2023-07-10 14:42:47 +0000

    fix: add JSON response for error exceptions

--- darkmoon/app.py
+++ darkmoon/app.py
@@ -4,9 +4,18 @@
 # Proprietary and confidential.
 
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse as jrp
 
 import darkmoon.api.v1.metadata.views as views
+from darkmoon.api.v1.metadata.Exception_Response import (
+    Duplicate_File_Exception,
+    Invalid_ID_Exception,
+    Item_Not_Found_Exception,
+    Missing_Hash_Exception,
+    Missing_Hash_Type_Exception,
+    Server_Not_Found_Exception,
+)
 from darkmoon.core.database import register_database
 from darkmoon.settings import Settings
 
@@ -17,7 +26,65 @@ def get_app(settings: Settings | None = None) -> FastAPI:
 
     app = FastAPI()
 
+    @app.exception_handler(Missing_Hash_Exception)
+    async def missingHashExcHndlr(rqt: Request, exc: Missing_Hash_Exception) -> jrp:
+        return jrp(
+            status_code=400,
+            content={"detail": "Enter Hash"},
+        )
+
+    @app.exception_handler(Missing_Hash_Type_Exception)
+    async def msngHashTpExcHndlr(rqt: Request, exc: Missing_Hash_Type_Exception) -> jrp:
+        return jrp(
+            status_code=400,
+            content={"detail": "Enter hash type"},
+        )
+
+    @app.exception_handler(Invalid_ID_Exception)
+    async def invalidIDExcHndlr(rqt: Request, exc: Invalid_ID_Exception) -> jrp:
+        return jrp(
+            status_code=400,
+            content={
+                "detail": (
+                    "This is not a valid ID",
+                    "It must be a 12-byte input or a 24-character hex string.",
+                ),
+            },
+        )
+
+    @app.exception_handler(Server_Not_Found_Exception)
+    async def srvrNoFoundExcHndlr(rqt: Request, exc: Server_Not_Found_Exception) -> jrp:
+        return jrp(
+            status_code=408,
+            content={
+                "detail": (
+                    "The computer can't find the server.",
+                    "Check the IP Address and the server name.",
+                ),
+            },
+        )
+
+    @app.exception_handler(Item_Not_Found_Exception)
+    async def itemNotFoundExcHndlr(rqt: Request, exc: Item_Not_Found_Exception) -> jrp:
+        return jrp(
+            status_code=404,
+            content={"detail": "Item not found"},
+        )
+
+    @app.exception_handler(Duplicate_File_Exception)
+    async def duplicateFileExcHndlr(rqt: Request, exc: Duplicate_File_Exception) -> jrp:
+        return jrp(
+            status_code=409,
+            content={"detail": "There is a duplicate file."},
+        )
+
     app.on_event("startup")(register_database(app, str(app_settings.MONGODB_CONN)))
+    app.add_exception_handler(Missing_Hash_Exception, missingHashExcHndlr)
+    app.add_exception_handler(Missing_Hash_Type_Exception, msngHashTpExcHndlr)
+    app.add_exception_handler(Invalid_ID_Exception, invalidIDExcHndlr)
+    app.add_exception_handler(Server_Not_Found_Exception, srvrNoFoundExcHndlr)
+    app.add_exception_handler(Item_Not_Found_Exception, itemNotFoundExcHndlr)
+    app.add_exception_handler(Duplicate_File_Exception, duplicateFileExcHndlr)
 
     app.include_router(views.router)
     return app
