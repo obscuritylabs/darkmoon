d1ae76bae6a3b422679420d4c6528957c0a7b563
Sean Meyer <seanm0914@gmail.com>
2023-07-10 16:54:31 -0400

    test: add integration testing for the API

--- tests/unit/api/test_api.py
+++
@@ -1,9 +0,0 @@
-import schemathesis
-
-schema = schemathesis.from_uri("http://localhost:8000/openapi.json")
-
-
-@schema.parametrize()  # type: ignore
-def test_api(case) -> None:
-    """Does stuff."""
-    case.call_and_validate()
