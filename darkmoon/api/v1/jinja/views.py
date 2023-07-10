0cce8e755d64582f76dcf770bcec30a854d219c7
OLabs Intern <mridulathulasiraman3@gmail.com>
2023-07-10 15:13:22 -0400

    feat: conneced the jinja with the html and css.

--- darkmoon/api/v1/jinja/views.py
+++ darkmoon/api/v1/jinja/views.py
@@ -10,4 +10,4 @@ templates = Jinja2Templates(directory="templates")
 @router.get("/items/{id}", response_class=HTMLResponse)
 async def read_item(request: Request, id: str) -> Response:
     """Read the request for response."""
-    return templates.TemplateResponse("item.html", {"request": request, "id": id})
+    return templates.TemplateResponse("index.html", {"request": request, "id": id})
