b3be77d5ee145d7238c6697cfcb85b290529b47c
Viraj Ganguli <viraj.ganguli06@gmail.com>
2023-07-12 09:30:30 -0400

    fix: Re-add previous changes to schema.py

--- darkmoon/core/schema.py
+++ darkmoon/core/schema.py
@@ -1,3 +1,4 @@
+from fastapi import HTTPException
 from pydantic import BaseModel, Field
 
 
@@ -7,3 +8,23 @@ class Response(BaseModel):
     message: str = Field(
         description="Base response model",
     )
+
+
+class ServerNotFoundException(HTTPException):
+    """Exception raised when the server is not found."""
+
+
+class ItemNotFoundException(HTTPException):
+    """Exception raised when an item is not found."""
+
+
+class DuplicateFileException(HTTPException):
+    """Exception raised when a duplicate file is encountered."""
+
+
+class IncorrectInputException(HTTPException):
+    """Exception raised when a input does not contain proper identifiers."""
+
+
+class InvalidIDException(HTTPException):
+    """Exception raised when an invalid ID is encountered."""
