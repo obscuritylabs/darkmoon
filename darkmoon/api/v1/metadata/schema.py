9d8d018b82ea180539d1b2d39fff52133b2b8d76
Sean Meyer <30555872+Synthint@users.noreply.github.com>
2023-07-17 18:05:02 -0400

    56 throw an exception for the case that there is missing input ie missing hashes (#71)
    * feat: add testing for api
    
    * test: add integration testing for the API
    
    * fix: move schemathesis to a dev dependency
    
    * feat: add testing for api
    
    * test: add integration testing for the API
    
    * fix: docker clean command
    
    * feat: add testing for api
    
    * test: add integration testing for the API
    
    * fix: adjust API schema to not allow empty lists or strings
    
    * fix: require file header info in API schema
    
    * feat: message
    
    * fix: Re-add previous changes to schema.py
    
    * fix: change error codes for Incorrect Inputs to 422
    
    * feat: add testing for api
    
    * test: add integration testing for the API
    
    * chore: add docker-in-docker
    
    * fix: Header info is now required when uploading a file
    
    * fix: POST metadata endpoint returns 201 status code
    
    * fix: validate inputs on GET request
    
    * chore: increase tests schemathesis runs and increase verbosity of errors
    
    * fix: add raised errors to API schema
    
    * fix: ensure all inputs to the database contain only valid UTF-8 characters
    
    * test: separated schemathesis tests from other tests, add make test-all
    
    * fix: ensure only valid utf8 is allowed in  GET requests
    
    * test: allow tests to pass if schemathesis tests fail
    
    * test: added static tests for get metadata endpoint
    
    * test: add api testing to ci pipeline
    
    * test: added static tests for get metadata/id endpoint
    
    * test: added static tests for post metadata endpoint
    
    * feat: add testing for api
    
    * fix: update command for schemathesis testing in the ci pipeline
    
    * Poetry pipe fix (#69)
    
    * fix: poetry lock update
    
    * fix: remove poetry-lock from the pipeline
    
    * fix: fix endpoint docstrings and responses
    
    * fix: regenerate lock file and fix rebase mistakes
    
    ---------
    
    Co-authored-by: Paige Maute <paige.maute@gmail.com>
    Co-authored-by: Dominique Waldon <dominiquekawaldon@gmail.com>
    Co-authored-by: Viraj Ganguli <viraj.ganguli06@gmail.com>

--- darkmoon/api/v1/metadata/schema.py
+++ darkmoon/api/v1/metadata/schema.py
@@ -1,29 +1,29 @@
 """Imports the modules/classes Field, BaseModel, Core Response model, and Optional."""
 from beanie import PydanticObjectId
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, validator
 
-from darkmoon.core.schema import Response
+from darkmoon.core.schema import IncorrectInputException, Response
 
 
 class Hashes(BaseModel):
     """Holds the hash info and is called in MetaData and MetaDataEntity."""
 
-    md5: str | None = Field(
+    md5: str = Field(
         description="The md5 hash",
         example="5d41402abc4b2a76b9719d911017c592",
     )
 
-    sha1: str | None = Field(
+    sha1: str = Field(
         description="The sha1 hash",
         example="aaf4c61ddcc5e8a2dabede0f3b482cd9aea9434d",
     )
 
-    sha256: str | None = Field(
+    sha256: str = Field(
         description="The sha256 hash",
         example="2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824",
     )
 
-    sha512: str | None = Field(
+    sha512: str = Field(
         description="The sha512 hash",
         example="75d527c368f2efe848ecd5f984f036eb6df891d75f72d9b154518c1cd58835286d1da9a38deba3de98b5a53e5ed78a84976",
     )
@@ -32,27 +32,27 @@ class Hashes(BaseModel):
 class HeaderInfo(BaseModel):
     """Holds HeaderInfo class properties for .exe files."""
 
-    machine_type: str | None = Field(
+    machine_type: str = Field(
         description="The machine type of the .exe file.",
         example="0x14c",
     )
 
-    timestamp: str | None = Field(
+    timestamp: str = Field(
         description="timestap of file",
         example="12/2/23 17:57:43",
     )
 
-    compile_time: str | None = Field(
+    compile_time: str = Field(
         description="compile time of the file",
         example="",
     )
 
-    signature: str | None = Field(
+    signature: str = Field(
         description="digital file signature",
         example="",
     )
 
-    rich_header_hashes: Hashes | None = Field(
+    rich_header_hashes: Hashes = Field(
         description="a dictionary of hashes from the hashes class",
     )
 
@@ -63,14 +63,20 @@ class Metadata(BaseModel):
     name: list[str] = Field(
         description="name of file",
         example=["End_Of_The_World"],
+        min_items=1,
+        regex="^(?!\\s*$).+",
     )
     file_extension: list[str] = Field(
         description="the extension of a file",
         example=[".jpeg"],
+        min_items=1,
+        regex="^(?!\\s*$).+",
     )
     file_type: list[str] = Field(
         description="the type of file",
         example=["exe"],
+        min_items=1,
+        regex="^(?!\\s*$).+",
     )
     hashes: Hashes = Field(
         description="a hash",
@@ -79,17 +85,40 @@ class Metadata(BaseModel):
     source_iso_name: list[str] = Field(
         description="source ISO name",
         example=[""],
+        min_items=1,
+        regex="^(?!\\s*$).+",
     )
 
     operating_system: list[str] = Field(
         description="The operating system of the computer"
         " where the file is coming from.",
         example=["WindowsXP"],
+        min_items=1,
+        regex="^(?!\\s*$).+",
     )
 
-    header_info: HeaderInfo | None = Field(
+    header_info: HeaderInfo = Field(
         description="contains all the header information",
     )
+
+    @validator(
+        "name",
+        "file_extension",
+        "file_type",
+        "source_iso_name",
+        "operating_system",
+    )
+    def validate_input(cls, input: list[str]) -> list[str]:
+        """Ensure all data in an uploaded file uses proper UTF-8 characters."""
+        for value in input:
+            try:
+                value.encode("UTF-8")
+            except UnicodeEncodeError:
+                raise IncorrectInputException(
+                    status_code=422,
+                    detail=("Input contains invalid characters"),
+                )
+        return input
 
 
 class MetadataEntity(BaseModel):
--- darkmoon/api/v1/metadata/views.py
+++ darkmoon/api/v1/metadata/views.py
@@ -1,24 +1,62 @@
+<<<<<<< HEAD
 """Defines an API router for handling metadata related requests."""
+=======
+"""This is the router file."""
+
+###########
+# IMPORTS #
+###########
+>>>>>>> 6f45317 (56 throw an exception for the case that there is missing input ie missing hashes (#71))
 
 import bson
 from beanie import PydanticObjectId
-from fastapi import APIRouter, Depends, HTTPException, Query
+from fastapi import APIRouter, Depends, Query, status
 from motor.motor_asyncio import AsyncIOMotorCollection
 from pymongo import errors
 
 from darkmoon.api.v1.metadata.schema import Metadata, MetadataEntity, UploadResponse
 from darkmoon.core.database import get_file_metadata_collection
+from darkmoon.core.schema import (
+    DuplicateFileException,
+    IncorrectInputException,
+    InvalidIDException,
+    ItemNotFoundException,
+    ServerNotFoundException,
+)
 
 router = APIRouter(prefix="/metadata", tags=["metadata"])
 
+<<<<<<< HEAD
+=======
 
-@router.get("/")
+###########
+# CLASSES #
+
+
+#############
+# FUNCTIONS #
+#############
+
+>>>>>>> 6f45317 (56 throw an exception for the case that there is missing input ie missing hashes (#71))
+
+@router.get(
+    "/",
+    responses={
+        422: {"Client Error Response": "Unprocessable Content"},
+        504: {"Server Error Response": "Gateway Timeout"},
+    },
+)
 async def list_metadata(
+    hash_type: str,
+    hash: str,
     collection: AsyncIOMotorCollection = Depends(get_file_metadata_collection),
     file_name: str | None = None,
-    hash_type: str | None = None,
-    hash: str | None = None,
-    page: int = Query(0, ge=0, description="The page to iterate to."),
+    page: int = Query(
+        0,
+        ge=0,
+        le=18446744073709552,
+        description="The page to iterate to.",
+    ),
     length: int = Query(10, ge=1, le=500),
 ) -> list[MetadataEntity]:
     """Get list of metadata that matches the parameters in the database.
@@ -39,7 +77,29 @@ async def list_metadata(
         List[MetadataEntity]: List of all documents that match parameters in the
         database.
 
+    Raises:
+        IncorrectInputException:
+            The input contains invalid UTF-8 characters
+            or is all white space
+        ServerNotFoundException:
+            Endpoint is unable to connect to mongoDB instance
+
     """
+    try:
+        hash.encode("UTF-8")
+        hash_type.encode("UTF-8")
+    except UnicodeEncodeError:
+        raise IncorrectInputException(
+            status_code=422,
+            detail=("Input contains invalid characters"),
+        )
+
+    if "\x00" in hash or "\x00" in hash_type or hash.isspace() or hash_type.isspace():
+        raise IncorrectInputException(
+            status_code=422,
+            detail=("Input contains invalid characters"),
+        )
+
     search = {}
 
     try:
@@ -49,23 +109,24 @@ async def list_metadata(
             hash_parameter = "hashes." + str(hash_type)
             search[hash_parameter] = hash
         elif hash_type:
-            raise HTTPException(status_code=400, detail="Enter hash")
+            raise IncorrectInputException(status_code=422, detail="Incorrect input.")
         elif hash:
-            raise HTTPException(status_code=400, detail="Enter hash type")
+            raise IncorrectInputException(status_code=422, detail="Incorrect input")
         data = await collection.find(search).skip(page * length).to_list(length=length)  # type: ignore # noqa
         return [MetadataEntity.parse_obj(item) for item in data]
 
     except errors.ServerSelectionTimeoutError:
-        raise HTTPException(
-            status_code=408,
-            detail=(
-                "The computer can't find the server.",
-                "Check the IP Address and the server name.",
-            ),
-        )
+        raise ServerNotFoundException(status_code=504, detail="Server timed out.")
 
 
-@router.get("/{id}")
+@router.get(
+    "/{id}",
+    responses={
+        400: {"Client Error Response": "Bad Request"},
+        404: {"Client Error Response": "Not Found"},
+        500: {"Server Error Response": "Internal Server Error"},
+    },
+)
 async def get_metadata_by_id(
     id: PydanticObjectId,
     collection: AsyncIOMotorCollection = Depends(get_file_metadata_collection),
@@ -74,15 +135,26 @@ async def get_metadata_by_id(
 
     Parameters:
         id (str): Unique id of specific entry in MongoDB
+<<<<<<< HEAD
         collection (AsyncIOMotorCollection) : The database collection to query.
 
     Raises:
         errors.ServerSelectionTimeoutError: If the server is not found.
         bson.errors.InvalidId: If the ID provided is invalid.
+=======
+>>>>>>> 6f45317 (56 throw an exception for the case that there is missing input ie missing hashes (#71))
 
     Returns:
         document (MetadataEntity): Return the database entry with
-            matching id or raise 404 error
+            matching id or raise 400, 404, or 500 error.
+
+    Raises:
+        InvalidIDException:
+            Provided ID is invalid
+        ItemNotFoundException:
+            no item with the provided ID is in the database
+        ServerNotFoundException:
+            Endpoint is unable to connect to mongoDB instance
 
     """
     try:
@@ -90,29 +162,26 @@ async def get_metadata_by_id(
         if doc:
             document = MetadataEntity(**doc)
         else:
-            raise HTTPException(status_code=404, detail="Item not found")
+            raise ItemNotFoundException(status_code=404, detail="Item not found.")
 
         return document
 
     except errors.ServerSelectionTimeoutError:
-        raise HTTPException(
-            status_code=408,
-            detail=(
-                "The computer can't find the server.",
-                "Check the IP Address and the server name.",
-            ),
-        )
+        raise ServerNotFoundException(status_code=504, detail="Server timed out.")
+
     except bson.errors.InvalidId:  # type: ignore
-        raise HTTPException(
-            status_code=400,
-            detail=(
-                "This is not a valid ID.",
-                "It must be a 12-byte input or a 24-character hex string.",
-            ),
-        )
+        raise InvalidIDException(status_code=400, detail="invalid ID.")
 
 
-@router.post("/")
+@router.post(
+    "/",
+    status_code=status.HTTP_201_CREATED,
+    responses={
+        409: {"Client Error Response": "Conflict"},
+        422: {"Client Error Response": "Unprocessable Content"},
+        500: {"Server Error Response": "Internal Server Error"},
+    },
+)
 async def upload_metadata(
     file: Metadata,
     collection: AsyncIOMotorCollection = Depends(get_file_metadata_collection),
@@ -120,14 +189,32 @@ async def upload_metadata(
     """Fast API POST function for incoming files.
 
     Parameters:
+<<<<<<< HEAD
         file (Metadata): The metadata of the file being uploaded.
         collection (AsyncIOMotorCollection): The database collection to insert the
         metadata into.
+=======
+        file (Metadata): The file that is uploaded to the database.
+
+    Returns:
+        response (UploadResponse): return a copy of the uploaded file
+            or raise 409, 422, or 500 error.
+
+    Raises:
+        DuplicateFileException:
+            A file already exists in the database with this information
+        IncorrectInputException:
+            Provided document is missing information or
+            uses invalid characters
+        ServerNotFoundException:
+            Endpoint is unable to connect to mongoDB instance
+>>>>>>> 6f45317 (56 throw an exception for the case that there is missing input ie missing hashes (#71))
 
     Raises:
         errors.ServerSelectionTimeoutError: If the server is not found.
     """
     file_metadata = file.dict()
+<<<<<<< HEAD
     duplicate_hashes = {
         "hashes.md5": file_metadata["hashes"]["md5"],
         "hashes.sha1": file_metadata["hashes"]["sha1"],
@@ -143,11 +230,31 @@ async def upload_metadata(
         "operating_system": file_metadata["operating_system"][0],
         "header_info": file_metadata["header_info"],
     }
+=======
+    try:
+        duplicate_hashes = {
+            "hashes.md5": file_metadata["hashes"]["md5"],
+            "hashes.sha1": file_metadata["hashes"]["sha1"],
+            "hashes.sha256": file_metadata["hashes"]["sha256"],
+            "hashes.sha512": file_metadata["hashes"]["sha512"],
+        }
+        check_dup = {
+            "name": file_metadata["name"][0],
+            "file_extension": file_metadata["file_extension"][0],
+            "file_type": file_metadata["file_type"][0],
+            "hashes": file_metadata["hashes"],
+            "source_iso_name": file_metadata["source_iso_name"][0],
+            "operating_system": file_metadata["operating_system"][0],
+            "header_info": file_metadata["header_info"],
+        }
+    except IndexError:
+        raise IncorrectInputException(status_code=422, detail=["Input missing"])
+>>>>>>> 6f45317 (56 throw an exception for the case that there is missing input ie missing hashes (#71))
 
     try:
         dup = await collection.find_one(check_dup)
         if dup:
-            raise HTTPException(status_code=409, detail=("There is a duplicate file."))
+            raise DuplicateFileException(status_code=409, detail="File is a duplicate.")
 
         doc = await collection.find_one(duplicate_hashes)
         if doc:
@@ -181,17 +288,16 @@ async def upload_metadata(
                 },
             }
             await collection.update_one(duplicate_hashes, change)
-            return UploadResponse(message="Successfully Updated Object", data=file)
+            return UploadResponse(message="Successfully Updated Object.", data=file)
 
         else:
             await collection.insert_one(file_metadata)
-            return UploadResponse(message="Successfully Inserted Object", data=file)
+            return UploadResponse(message="Successfully Inserted Object.", data=file)
 
     except errors.ServerSelectionTimeoutError:
-        raise HTTPException(
-            status_code=408,
-            detail=(
-                "The computer can't find the server.",
-                "Check the IP Address and the server name.",
-            ),
+        raise ServerNotFoundException(status_code=500, detail="Server not found.")
+    except UnicodeEncodeError:
+        raise IncorrectInputException(
+            status_code=422,
+            detail=("Input contains invalid characters"),
         )
