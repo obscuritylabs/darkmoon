4831f078b446eb202328b768d0c0d90d4949a1aa
Sean Meyer <seanm0914@gmail.com>
2023-07-20 15:17:05 -0400

    feat: allow ISO upload to proxmox server

--- darkmoon/settings.py
+++ darkmoon/settings.py
@@ -3,10 +3,15 @@
 # All rights reserved. No warranty, explicit or implicit, provided.
 # Proprietary and confidential.
 
+
 from pydantic import BaseSettings, Field, MongoDsn
 
 
-class Settings(BaseSettings):
+class Settings(BaseSettings):  # type: ignore
     """The Settings class."""
 
     MONGODB_CONN: MongoDsn = Field(default="mongodb://darkmoon:password@mongodb:27017/?authMechanism=DEFAULT")  # type: ignore  # noqa
+
+    PROXMOX_PASS: str = Field(default="")
+    PROXMOX_USER: str = Field(default="")
+    PROXMOX_HOST: str = Field(default="")
---
+++ darkmoon/vm_utils.py
@@ -0,0 +1,18 @@
+from proxmoxer import ProxmoxAPI
+
+from darkmoon.settings import Settings
+
+prox = ProxmoxAPI(
+    Settings.PROXMOX_HOST,
+    user=Settings.PROXMOX_USER,
+    password=Settings.PROXMOX_PASS,
+    verify_ssl=False,
+)
+
+
+def upload_iso(node: str, storage: str, file_path: str) -> None:
+    """Upload a provided iso to a given node and storage."""
+    prox.proxmox.nodes(node).storage(storage).upload.post(
+        content="iso",
+        filename=open(file_path, "rb"),
+    )
