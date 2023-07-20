8f3adfbec209e85c79e48cf48113181fb467907c
OLabs Intern <mridulathulasiraman3@gmail.com>
2023-07-20 13:55:16 -0400

    feat: made tests to detmine if jinja is acessesing the right html and css file

---
+++ tests/integration/api/test_jinja.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from fastapi.testclient import TestClient
+
+
+def test_jinja(populated_app: FastAPI) -> None:
+    """Test jinja webpages endpoint."""
+    with TestClient(populated_app) as app:
+        response = app.get(
+            "/webpages/",
+        )
+        assert (
+            '<link href="http://testserver/static/styles.css" rel="stylesheet">'
+            in response.content.decode("utf-8")
+        )
