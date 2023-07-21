e1d1befa23dd87c2fa1a96b71e05e2778a55de83
Dominique Waldon <dominiquekawaldon@gmail.com>
2023-07-21 13:33:58 -0400

    feat: Created webpage for hashlookup

--- darkmoon/api/v1/jinja/views.py
+++ darkmoon/api/v1/jinja/views.py
@@ -7,6 +7,31 @@ router = APIRouter(prefix="/webpages", tags=["webpages"])
 
 
 @router.get("", response_class=HTMLResponse)
-async def read_item(request: Request) -> Response:
-    """Read the request for response."""
+async def read_item_index(request: Request) -> Response:
+    """Read the request from index page for response."""
     return templates.TemplateResponse("index.html", {"request": request})
+
+
+@router.get("", response_class=HTMLResponse)
+async def read_item_hash(request: Request) -> Response:
+    """Read the request from index page for response."""
+    return templates.TemplateResponse("hash.html", {"request": request})
+
+
+@router.post("/process_data", response_class=HTMLResponse)
+async def process_data(
+    request: Request,
+    hash: str,
+    hash_type: str,
+    file: str,
+) -> Response:
+    """Will change."""
+    return templates.TemplateResponse(
+        "result.html",
+        {
+            "request": Request,
+            "hash": hash,
+            "hash_type": hash_type,
+            "file_name": file,
+        },
+    )
