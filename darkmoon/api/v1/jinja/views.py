6b14beebe4572e046cfa322ef05d7d4a13100776
Dominique Waldon <dominiquekawaldon@gmail.com>
2023-07-24 17:02:06 -0400

    feat: Made changes to new homepage

--- darkmoon/api/v1/jinja/views.py
+++ darkmoon/api/v1/jinja/views.py
@@ -7,6 +7,18 @@ router = APIRouter(prefix="/webpages", tags=["webpages"])
 
 
 @router.get("", response_class=HTMLResponse)
-async def read_item(request: Request) -> Response:
+async def read_item_index(request: Request) -> Response:
     """Read the request for response."""
     return templates.TemplateResponse("index.html", {"request": request})
+
+
+@router.get("/hash-lookup", response_class=HTMLResponse)
+async def read_item_hash(request: Request) -> Response:
+    """Read the request for response."""
+    return templates.TemplateResponse("hash.html", {"request": request})
+
+
+@router.get("/upload", response_class=HTMLResponse)
+async def read_item_upload(request: Request) -> Response:
+    """Read the request for response."""
+    return templates.TemplateResponse("upload.html", {"request": request})
