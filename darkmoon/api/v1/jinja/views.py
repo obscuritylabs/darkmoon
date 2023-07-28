4b4d2729b8965ef68358c5ab4c7b3e94589d75df
OLabs Intern <mridulathulasiraman3@gmail.com>
2023-07-28 10:59:36 -0400

    feat: working on getting the output in the webpage

--- darkmoon/api/v1/jinja/views.py
+++ darkmoon/api/v1/jinja/views.py
@@ -1,10 +1,10 @@
 # views.py
 
-from fastapi import APIRouter, File, Request, UploadFile
+from fastapi import APIRouter, File, Path, Request, UploadFile
 from fastapi.responses import HTMLResponse, Response
 
 import darkmoon.common.name
-from darkmoon.settings import HERE, templates
+from darkmoon.settings import templates
 
 router = APIRouter(prefix="/webpages", tags=["webpages"])
 
@@ -30,13 +30,24 @@ async def read_item_upload(request: Request) -> Response:
 @router.post("/upload", response_class=HTMLResponse)
 async def upload_vmdk(request: Request, file: UploadFile = File(...)) -> Response:
     """Handle VMDK file upload and processing."""
-    file_path = f"{HERE}/{darkmoon.common.name}"
+    upload_dir = Path("path/to/your/upload/directory")
+    upload_dir.mkdir(parents=True, exist_ok=True)
+
+    file_path = upload_dir / file.filename
     with open(file_path, "wb") as f:
         f.write(await file.read())
 
+    # Extract the filename from the uploaded file
+    extracted_filename = file.filename
+
+    # Process the VMDK file
     results = darkmoon.common.name.process_vmdk(file_path)
 
     return templates.TemplateResponse(
-        "results.html",
-        {"request": request, "results": results},
+        "upload.html",
+        {
+            "request": request,
+            "extracted_filename": extracted_filename,
+            "results": results,
+        },
     )
